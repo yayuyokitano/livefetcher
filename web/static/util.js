92c5f98dc471ce770cfda32d934643887ee1d2e3
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2024-08-18 19:16:08 +0900

    Add formatting for web code

--- web/static/util.js
+++ web/static/util.js
@@ -2,17 +2,17 @@ const userButton = document.querySelectorAll(".user-button");
 const userPopup = document.querySelectorAll(".user-popup");
 
 userButton.forEach((btn) => {
-	btn.addEventListener("click", () => {
-		btn.nextElementSibling.show();
-	});
+  btn.addEventListener("click", () => {
+    btn.nextElementSibling.show();
+  });
 });
 
 document.addEventListener("click", (e) => {
-	if (e.target.closest(".dialog-button-wrapper")) {
-		return;
-	}
+  if (e.target.closest(".dialog-button-wrapper")) {
+    return;
+  }
 
-	userPopup.forEach((popup) => {
-		popup.close();
-	});
+  userPopup.forEach((popup) => {
+    popup.close();
+  });
 });
